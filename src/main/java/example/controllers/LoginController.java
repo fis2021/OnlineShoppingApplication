1f097c7a85b7b3144ebb18e353f35444452c1579
Roxy563 <57462147+Roxy563@users.noreply.github.com>
2021-05-12 23:40:49 +0300

    Some changes added.

--- src/main/java/example/controllers/LoginController.java
+++ src/main/java/example/controllers/LoginController.java
@@ -15,6 +15,8 @@ import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.io.PrintStream;
+
 
 public class LoginController {
 
@@ -47,20 +49,22 @@ public class LoginController {
     private void login_check(ActionEvent actionEvent) throws IOException {
 
         int count = 0;
-        User user;;
+        User user;
         try {
             user = UserService.checkCorrectPassword(EmailField.getText(), PasswordField.getText());
             Parent root;
-            root = FXMLLoader.load(getClass().getResource("/fxml/seller.fxml"));
+            if(user.getRole().equals("Seller"))
+                root = FXMLLoader.load(getClass().getResource("/seller.fxml"));
+            else root = FXMLLoader.load(getClass().getResource("/buyer.fxml"));
             Scene scene = new Scene(root,600,500);
             Stage stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
-            stage.setTitle("Seller");
+            stage.setTitle("Welcome!");
             stage.setScene(scene);
             stage.show();
 
         } catch (UserDoesNotExist e) {
             count++;
-            // login_test.setText(e.getMessage());
+//            login_test.setText(e.getMessage());
         } catch (IncorrectPasswordException e) {
             login_test.setText(e.getMessage());
         }
@@ -74,12 +78,13 @@ public class LoginController {
         }
     }
 
+
     public void onToRegistration(ActionEvent event) throws IOException {
         Parent p= FXMLLoader.load(getClass().getResource("/register.fxml"));
-        Scene scene0=new Scene(p,600,500);
+        Scene scene22=new Scene(p,600,500);
         Stage window=(Stage)((Node)event.getSource()).getScene().getWindow();
-        window.setTitle("Login page");
-        window.setScene(scene0);
+        window.setTitle("Register page");
+        window.setScene(scene22);
         window.show();
     }
 }
